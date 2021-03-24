a63730ebb8d320f2f0566aee758efac190d84751
Vijay <asp.vijaybaskar@gmail.com>
2021-03-24 23:28:51 +0530

    Routing implemented routing works

--- src/server.js
+++ src/server.js
@@ -9,6 +9,12 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+// define routes
+app.use("/api/users", require("./api/users"));
+app.use("/api/profile", require("./api/profile"));
+app.use("/api/posts", require("./api/posts"));
+app.use("/api/auth", require("./api/auth"));
+
 app.listen(PORT, () => {
   console.log("Application is listening on PORT number :", PORT);
 });
