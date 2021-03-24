258fb96a4a513a9c727c2b293a8acce458efcef9
Vijay <asp.vijaybaskar@gmail.com>
2021-03-24 19:52:25 +0530

    Db connected but collections are not yet created pass v*******3

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 5000;
 
+var connectDB = require("../config/db");
+
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
