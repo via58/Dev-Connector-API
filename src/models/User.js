0ce717cf023cf4351d33a9e80c77f5e9c49d7907
Vijay <asp.vijaybaskar@gmail.com>
2021-03-25 13:52:33 +0530

    user registeration implemented user registration user existeance check gravatar bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -22,4 +22,6 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   }
 });
-module.exports = mongoose.model("user", userSchema);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
