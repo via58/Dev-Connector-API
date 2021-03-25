23dfe0d87398b0b00aabe804c3ce49981c260d06
Vijay <asp.vijaybaskar@gmail.com>
2021-03-25 17:55:07 +0530

    Auth middleware implemented

---
+++ middleware/Auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    res.status(401).json({ msg: "Authorization denied" });
+  }
+  try {
+    let decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    console.log(err.message);
+    res.status(401).json({ msg: "token is Invalid" });
+  }
+};
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const Authmiddleware = require("../../middleware/Auth");
 
 // GET method
 // auth route
 // /api/auth
-router.get("/", (req, res) => {
+router.get("/", Authmiddleware, (req, res) => {
   res.send("Auth route");
 });
 module.exports = router;
