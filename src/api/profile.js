fdaddc8540b4a23bf13dff5759aab8a63768e7bb
Vijay <asp.vijaybaskar@gmail.com>
2021-03-28 19:45:59 +0530

    Profile Education implemented

--- src/api/profile.js
+++ src/api/profile.js
@@ -9,7 +9,7 @@ router.get("/me", [AuthMiddleware], async (req, res) => {
   try {
     let profiles = await ProfileModel.findOne({
       user: req.user
-    }).populate("user", ["name", "avatars"]);
+    }).populate("User", ["name", "avatars"]);
     if (!profiles) {
       return res.status(400).json({ msg: "Profile not found" });
     }
@@ -101,7 +101,7 @@ router.post(
 
 router.get("/", async (req, res) => {
   try {
-    const profiles = await ProfileModel.find().populate("user", [
+    const profiles = await ProfileModel.find().populate("User", [
       "name",
       "avatar"
     ]);
@@ -124,7 +124,7 @@ router.get("/user/:user_id", async (req, res) => {
   try {
     const profile = await ProfileModel.findOne({
       user: req.params.user_id
-    }).populate("user", ["name", "avatar"]);
+    }).populate("User", ["name", "avatar"]);
     if (!profile) {
       return res.status(404).json({ msg: "Profile not found" });
     }
@@ -136,6 +136,186 @@ router.get("/user/:user_id", async (req, res) => {
       res.status(404).json({ msg: "Profile not found" });
     }
     res.status(500).send("Server Error");
+  }
+});
+
+//@method get  profiles by token
+//@access private
+//@desc   get  profile data by token
+
+router.delete("/", AuthMiddleware, async (req, res) => {
+  try {
+    // Delete profile
+    await ProfileModel.findOneAndRemove({ user: req.user });
+    //Delete User
+    await User.findOneAndRemove({ _id: req.user });
+
+    //to-do delete respective post
+
+    res.json("Profile deleted");
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send(" Server Error");
+  }
+});
+
+//@method put  profile experience by token
+//@access private
+//@desc   put  profile experience data by token
+
+router.put(
+  "/experience",
+  [
+    AuthMiddleware,
+    [
+      check("title", "title should not be empty").not().isEmpty(),
+      check("from", "from date should be valid").not().isEmpty(),
+      check("company", "company name should not be empty").not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    try {
+      const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+      } = req.body;
+      // get profile
+      const profile = await ProfileModel.findOne({ user: req.user });
+      //add experience
+      const experienceObject = {
+        title: title,
+        company: company,
+        location: location,
+        from: from,
+        to: to,
+        current: current,
+        description: description
+      };
+
+      profile.experience.unshift(experienceObject);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send(" Server Error");
+    }
+  }
+);
+
+//@method delete  profiles Experience by experience id
+//@access private
+//@desc   delete  profiles Experience by experience id
+
+router.delete("/experience/:exp_id", AuthMiddleware, async (req, res) => {
+  try {
+    // get profile
+    const profile = await ProfileModel.findOne({ user: req.user });
+
+    //get index of specific experience
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    // remove expereince from profile
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send(" Server Error");
+  }
+});
+
+//@method put  profile Education by token
+//@access private
+//@desc   put  profile Education data by token
+
+router.put(
+  "/education",
+  [
+    AuthMiddleware,
+    [
+      check("school", "school name should not be empty").not().isEmpty(),
+      check("from", "from date should be valid").not().isEmpty(),
+      check("degree", "degree of study should not be empty").not().isEmpty(),
+
+      check("fieldofstudy", "field of study should not be empty")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    try {
+      const {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+      } = req.body;
+      // get profile
+      const profile = await ProfileModel.findOne({ user: req.user });
+      //add education
+      const educationObject = {
+        school: school,
+        degree: degree,
+        fieldofstudy: fieldofstudy,
+        from: from,
+        to: to,
+        current: current,
+        description: description
+      };
+
+      profile.education.unshift(educationObject);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send(" Server Error");
+    }
+  }
+);
+
+//@method delete  profiles Education by experience id
+//@access private
+//@desc   delete  profiles Education by experience id
+
+router.delete("/education/:edu_id", AuthMiddleware, async (req, res) => {
+  try {
+    // get profile
+    const profile = await ProfileModel.findOne({ user: req.user });
+
+    //get index of specific experience
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    // remove education from profile
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send(" Server Error");
   }
 });
 
