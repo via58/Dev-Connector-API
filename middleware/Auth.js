f6ad7b23245e6b8653e0d3a5bce544ee7cdd06f7
Vijay <asp.vijaybaskar@gmail.com>
2021-03-28 12:32:08 +0530

    Defect fix

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -8,9 +8,9 @@ module.exports = function (req, res, next) {
   }
   try {
     let decoded = jwt.verify(token, config.get("jwtSecret"));
-    req.user = decoded.user;
-    console.log(decoded);
-    // console.log(req);
+    req.user = decoded.user === undefined ? decoded.id : decoded.user;
+    console.log("docoded", decoded);
+    console.log("req.user", req.user);
 
     next();
   } catch (err) {
